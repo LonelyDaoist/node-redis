327c27e7f305bd2044a8c48c7609f5366ff1da57
LonelyDaoist <ghabaroughassen@gmail.com>
2020-03-20 19:24:04 +0100

    done

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const redis = require('redis');
+
+const app = express();
+const client = redis.createClient({
+	host: 'cache',
+	port: process.env.CACHE_PORT
+});
+const port = process.env.PORT || 5000;
+
+client.set('count',0);
+
+app.get('/', async (req, res) => {
+	client.get('count', (err,count) => {
+		res.send(count);
+		client.set('count',parseInt(count)+1);
+	});
+});
+
+app.listen(port, () => console.log(`listening on port ${port}`));
